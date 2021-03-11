4619490a13302077336fb97297a65a9f2115b683
Henriette Holm <henriette.aamand.holm@live.dk>
2021-03-11 15:15:16 +0100

    skrevet implementeringen til Display_simulaor og logFile

---
+++ ChargeStation.Classlibrary/Display_Simulator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Channels;
+
+namespace ChargeStation.Classlibrary
+{
+    public class Display_Simulator : IDisplay
+    {
+        public void ChangeText(string indlæsRfid)
+        {
+            Console.WriteLine(indlæsRfid);
+        }
+    }
+}
--- ChargeStation.Classlibrary/Logfile.cs
+++ ChargeStation.Classlibrary/Logfile.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace ChargeStation.Classlibrary
 {
     public class Logfile : ILogfile
     {
+        private string filename = "Logfile";
+        private FileStream streamer;
+        private StreamWriter writer;
+
+        public Logfile()
+        {
+            if (!File.Exists(filename))
+            {
+                streamer = new FileStream("Log", FileMode.Create, FileAccess.Write);
+                streamer.Close();
+            }
+        }
+
+        public void DoorUnlocked(string rfid_Id)
+        {
+            string logLine = "Door was unlocked at " + DateTime.Now + " with Rfid id " + rfid_Id;
+            File.AppendAllText(filename, logLine);
+        }
+
+        public void DoorLocked(string rfid_Id)
+        {
+            string logLine = "Door was locked at " + DateTime.Now + " with Rfid id " + rfid_Id;
+            File.AppendAllText(filename, logLine);
+        }
     }
 }
