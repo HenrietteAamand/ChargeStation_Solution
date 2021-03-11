c3f7296f032b658ed4e4f017d1c58e8ac44a40d0
Mads Emil <58051332+memilnieAU@users.noreply.github.com>
2021-03-11 15:16:12 +0100

    Test er påbegyndt for DoorSimulator

--- ChargeStation.Classlibrary/UsbChargerSimulator.cs
+++ ChargeStation.Classlibrary/UsbChargerSimulator.cs
@@ -16,6 +16,8 @@ namespace ChargeStation.Classlibrary
 
         public double CurrentValue { get; private set; }
 
+        public event EventHandler<CurrentEventArgs> ValueEvent;
+        public double CorrentValue { get; }
         public bool Connected { get; private set; }
 
         private bool _overload;
