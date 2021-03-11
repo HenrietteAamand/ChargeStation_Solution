853f93ee983e19a020190918052f575c4a659227
Mads Emil <58051332+memilnieAU@users.noreply.github.com>
2021-03-11 14:13:22 +0100

    DoorSimulator er designet, men ikke testet

--- ChargeStation.Classlibrary/Door_Simulator.cs
+++ ChargeStation.Classlibrary/Door_Simulator.cs
@@ -4,7 +4,87 @@ using System.Text;
 
 namespace ChargeStation.Classlibrary
 {
-    class Door_Simulator
+    
+
+    public class Door_Simulator : IDoor
     {
+
+        public event EventHandler<DoorStatusEventArgs> DoorStatusChangedEvent;
+        
+        
+        private bool _doorIsOpen;
+
+        public bool DoorIsOpen
+        {
+            get { return _doorIsOpen; }
+            set
+            {
+                if (value != _doorIsOpen)
+                {
+                    _doorIsOpen = value;
+                    OnDoorStatusChanged(new DoorStatusEventArgs
+                    {
+                        IsOpen = value
+
+                    });
+                }
+            }
+        }
+
+        private void OnDoorStatusChanged(DoorStatusEventArgs eventArgs)
+        {
+            DoorStatusChangedEvent?.Invoke(this, eventArgs);
+        }
+
+        private bool _doorIsLocked;
+
+        public bool DoorIsLocked
+        {
+            get { return _doorIsLocked; }
+            set { _doorIsLocked = value; }
+        }
+
+        public void LockDoor()
+        {
+            if (!DoorIsOpen)
+            {
+                DoorIsLocked = true;
+            }
+        }
+
+        public void UnlockDoor()
+        {
+            DoorIsLocked = false;
+        }
+
+        public void SimulateOpeningTry()
+        {
+            if (DoorIsLocked)
+            {
+                throw new ArgumentException("Døren var låst da du åbnede..");
+            }
+            else
+            {
+                DoorIsOpen = true;
+            }
+        }
+
+        public void SimulateClosingTry()
+        {
+            if (DoorIsLocked)
+            {
+                throw new ArgumentException("Døren var låst da du lukkede..");
+            }
+            else
+            {
+                DoorIsOpen = false;
+            }
+        }
+
+    }
+
+    public class DoorStatusEventArgs : EventArgs
+    {
+        public bool IsOpen { get; set; }
     }
 }
---
+++ ChargeStation.Test.Unit/Door_Simulator_Test_Unit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+
+namespace ChargeStation.Test.Unit
+{
+    class Door_Simulator_Test_Unit
+    {
+    }
+}
