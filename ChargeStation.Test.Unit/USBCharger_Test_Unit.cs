cbc89c31db87f538c2479fe51e86d75ca69ae326
Emil Søndergaard <au603943@uni.au.dk>
2021-03-17 10:10:54 +0100

    Udkommenteret test

--- ChargeStation.Test.Unit/USBCharger_Test_Unit.cs
+++ ChargeStation.Test.Unit/USBCharger_Test_Unit.cs
@@ -66,14 +66,14 @@ namespace ChargeStation.Test.Unit
 
         #endregion
         #region OverLoadCurrent
-        [Test]
-        public void StartCharge_OverLoadCurrentIstrue_EventFired()
-        {
+        //[Test]
+        //public void StartCharge_OverLoadCurrentIstrue_EventFired()
+        //{
            
 
-            Assert.That(receivedEventArgs, Is.Not.Null);
+        //    Assert.That(receivedEventArgs, Is.Not.Null);
 
-        }
+        //}
 
         //[Test]
         //public void StartCharge_CurrentSetToNewValue_CorrectNewCurrentReceived()
