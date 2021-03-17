afd941504dccae639ee537651232dcfd59368153
Henriette Holm <henriette.aamand.holm@live.dk>
2021-03-17 08:58:02 +0100

    Tilpasset Logfile, så den får dato gennem timeprovider, og skriver til fil gennem filewriter

--- ChargeStation.Classlibrary/Logfile.cs
+++ ChargeStation.Classlibrary/Logfile.cs
@@ -9,31 +9,27 @@ namespace ChargeStation.Classlibrary
     {
         private readonly IFileWriter _fileWriter;
         private readonly ITimeProvider _timeprovider;
-        private string filename = "Logfile";
-        private FileStream streamer;
-        private StreamWriter writer;
 
         public Logfile(IFileWriter fileWriter, ITimeProvider timeprovider)
         {
             _fileWriter = fileWriter;
             _timeprovider = timeprovider;
-            if (!File.Exists(filename))
-            {
-                streamer = new FileStream("Log", FileMode.Create, FileAccess.Write);
-                streamer.Close();
-            }
         }
 
         public void DoorUnlocked(string rfid_Id)
         {
-            string logLine = "Door was unlocked at " + DateTime.Now + " with Rfid id " + rfid_Id;
-            File.AppendAllText(filename, logLine);
+            string time = _timeprovider.GetTime();
+
+            string logLine = "Door was unlocked at " + time + " with Rfid id " + rfid_Id;
+
+            _fileWriter.WriteLineToFile(logLine);
         }
 
         public void DoorLocked(string rfid_Id)
         {
-            string logLine = "Door was locked at " + DateTime.Now + " with Rfid id " + rfid_Id;
-            File.AppendAllText(filename, logLine);
+            string time = _timeprovider.GetTime();
+            string logLine = "Door was locked at " + time + " with Rfid id " + rfid_Id;
+            _fileWriter.WriteLineToFile(logLine);
         }
     }
 }
