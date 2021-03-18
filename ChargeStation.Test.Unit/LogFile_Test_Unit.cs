b44ef3d02d5071f0852afecdf7f6f16dc819bdff
Henriette Holm <henriette.aamand.holm@live.dk>
2021-03-18 14:41:42 +0100

    Lavet to tests til LogFile - måske mangler der lidt zombies

---
+++ ChargeStation.Test.Unit/LogFile_Test_Unit.cs
@@ -0,0 +1,37 @@
+﻿using ChargeStation.Classlibrary;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ChargeStation.Test.Unit
+{
+    class LogFile_Test_Unit
+    {
+        private IFileWriter fakeFilewriter;
+        private ITimeProvider faketimeProvider;
+        private Logfile uut;
+        [SetUp]
+        public void Setup()
+        {
+            fakeFilewriter = Substitute.For<IFileWriter>();
+            faketimeProvider = Substitute.For<ITimeProvider>();
+            uut = new Logfile(fakeFilewriter, faketimeProvider);
+        }
+
+        // OBS! Skal jeg lave flere testcases?
+        [Test]
+        public void DoorUnlocked_SetTimeAndID_checkFileWriterLine()
+        {
+            faketimeProvider.GetTime().Returns("1234");
+            uut.DoorUnlocked("RFID_ID");
+            fakeFilewriter.Received(1).WriteLineToFile("Door was unlocked at 1234 with Rfid id RFID_ID");
+        }
+
+        [Test]
+        public void DoorLocked_SetTimeAndID_checkFileWriterLine()
+        {
+            faketimeProvider.GetTime().Returns("1234");
+            uut.DoorUnlocked("RFID_ID");
+            fakeFilewriter.Received(1).WriteLineToFile("Door was unlocked at 1234 with Rfid id RFID_ID");
+        }
+    }
+}
