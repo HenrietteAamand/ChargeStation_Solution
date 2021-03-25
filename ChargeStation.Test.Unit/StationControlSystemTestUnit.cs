2a16b34089f7cdd166adf6aec6b3958e149438ce
Mads Emil <58051332+memilnieAU@users.noreply.github.com>
2021-03-25 20:51:54 +0100

    Slettet overflødig test

--- ChargeStation.Test.Unit/StationControlSystemTestUnit.cs
+++ ChargeStation.Test.Unit/StationControlSystemTestUnit.cs
@@ -28,75 +28,7 @@ namespace ChargeStation.Test.Unit
 
 
         }
-        [Test]
-        public void TotalSystemTest_FollowSekvensDiagram10Times_Correct()
-        {
-
-            // sd HandleDoorEvent
-            // Vi åbner døren:
-            door.DoorStatusChangedEvent += Raise.EventWith(new DoorStatusEventArgs { IsOpen = true });
-
-
-            // Vi tester at døren er åben og beskeden Tilslut telefon udskrives:
-            display.Received(1).ChangeText(MessageCode.TilslutTelefon);
-
-            // Vi tilslutter telefonen og lukker døren:
-            door.DoorStatusChangedEvent += Raise.EventWith(new DoorStatusEventArgs { IsOpen = false });
-
-            #region Vi forventer at display viser besked om indlæs RFID
-
-            display.Received(1).ChangeText(MessageCode.IndlaesRFID);
-
-            rfdReader.RFIDDectectedEvent += Raise.EventWith(new RFIDDetectedEventArgs() { Id = "SpecialHashCode:aokjshfdciuosydcnakjsefdi" });
-            usbCharger.Received(0).StartCharge();
-            door.Received(0).LockDoor();
-            logfile.Received(0).DoorLocked("SpecialHashCode:aokjshfdciuosydcnakjsefdi");
-            display.Received(0).ChangeText(MessageCode.LadeskabOptaget);
-            display.Received(1).ChangeText(MessageCode.Tilslutningsfejl);
-
-            #endregion
-
-            #region Raiser event med RFID detected
-            // Raiser event med RFID detected
-            usbCharger.Connected.Returns(true); //Faker den returnerer true
-            rfdReader.RFIDDectectedEvent += Raise.EventWith(new RFIDDetectedEventArgs() { Id = "SpecialHashCode:aokjshfdciuosydcnakjsefdi" });
-            usbCharger.Received(1).StartCharge();
-            door.Received(1).LockDoor();
-            door.DoorIsLocked.Returns(true);
-            logfile.Received(1).DoorLocked("SpecialHashCode:aokjshfdciuosydcnakjsefdi");
-            display.Received(1).ChangeText(MessageCode.LadeskabOptaget);
-            // Skabet er nu optaget og vi lader med låst dør
-            #endregion
-
-            #region Forkert RFID tag prøver at få adgang til skabet
-            rfdReader.RFIDDectectedEvent += Raise.EventWith(new RFIDDetectedEventArgs() { Id = "SpecialHashCode:aokjshfdciuosydcnakjsefd0l" });
-            usbCharger.Received(0).StopCharge();
-            door.Received(0).UnlockDoor();
-            logfile.Received(0).DoorUnlocked("SpecialHashCode:aokjshfdciuosydcnakjsefdi");
-            display.Received(0).ChangeText(MessageCode.FjernTelefon);
-            //Skabet åbnede ikke!
-            #endregion
-
-            #region Vi vil hente vores telefon nu med korrekt RFID Tag.
-            // Vi vil hente vores telefon nu med korrekt RFID Tag.
-            rfdReader.RFIDDectectedEvent += Raise.EventWith(new RFIDDetectedEventArgs() { Id = "SpecialHashCode:aokjshfdciuosydcnakjsefdi" });
-            usbCharger.Received(1).StopCharge();
-            door.Received(1).UnlockDoor();
-            logfile.Received(1).DoorUnlocked("SpecialHashCode:aokjshfdciuosydcnakjsefdi");
-            display.Received(1).ChangeText(MessageCode.FjernTelefon);
-
-            #endregion
-
-            #region Vi åbner døren og tager telefonen
-
-            door.DoorStatusChangedEvent += Raise.EventWith(new DoorStatusEventArgs { IsOpen = true });
-
-            usbCharger.Connected.Returns(false);
-
-            #endregion
-
-        }
-
+       
         [Test]
         public void TotalSystemTest_IntendedUseOneTime_Correct()
         {
